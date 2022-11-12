c688e3247074aa54762a639b6fe7e131ad3010c1
Admin <iggla67@yandex.ru>
2022-11-12 13:22:31 +0400

    Первое задание получилось.Со вторым тоже самое.Класс занят,метод тоже .Как втиснуть второе задание в Идею?

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,44 @@ public class Main {
     //Результат программы выведите в консоль. Если год високосный,
     // то должно быть выведено “*номер года
     //   —* високосный год”. Если год не високосный, то,
-    //   соответственно: “*номер года —* не **високосный год”.
-    public static void main(String[] args) {
-        System.out.println("Задание 1");
-        int year = 2021;
+    //   соответственно: “*номер года —* не **високосный год”
+
+    public static void findOutLeapYear() {
+        int year = 2022;
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("Год является високосным.");
         } else {
             System.out.println("Год не является високосным");
         }
     }
+    public static void main(String[] args) {
+        findOutLeapYear();
+    }
 }
-//Я НЕ ПОНИМАЮ ,КУДА Я ДОЛЖНА ПОСТАВИТЬ МЕТОД.??СВЕРХУ И ТАМ МАЙН И НИЖЕ ЕЩЕ МАЙН?
+    //Напишите метод, куда подаются два параметра: тип операционной системы
+    // (ОС) ( 0 — iOS или 1 — Androd) и год выпуска устройства.
+    //Текущий год можно получить таким способом:int currentYear = LocalDate.now().getYear();
+//В результате программа должна выводить в консоль в зависимости от исходных данных,
+// какую версию приложения (обычную или lite) и для какой ОС (Android или iOS)
+// нужно установить пользователю.
+    public static void installOs() {
+        int clientOs = 0;
+        int clientDeviceYear = 2019;
+        if (clientOs == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылкe .");
+            } else {
+                System.out.println("Установите  версию приложения для iOS по ссылкe .");
+            }
+        } else {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылкe .");
+            } else {
+                System.out.println("Установите  версию приложения для Android по ссылкe .");
+            }
+        }
+        public static void main (String[]args){
+            installOs();
+        }
+    }
+}
